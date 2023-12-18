38acadfd1b5db11b3ca3c58637c8bd61eec7cd17
929486 <kyle929486@gmail.com>
2023-12-18 11:07:16 +0900

    Initial commit

---
+++ src/main/java/com/lec/spring/domain/User.java
@@ -0,0 +1,30 @@
+package com.lec.spring.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private  Long id;
+    private String username;
+    @JsonIgnore // Json화 할 때 해당 프로퍼티 제외
+    private String password;
+    @ToString.Exclude // toString() 결과에서 뺌
+    @JsonIgnore
+    private String re_password; // 비밀번호 확인 입력
+    private String name;
+    private String email;
+    @JsonIgnore
+    private LocalDateTime regDate;
+
+    // OAuth2
+    private String provider;  // 어떤 OAuth2 제공자? Kakao, Naver, Google...
+    private String providerId;  // provider 내에서의 고유 id 값
+
+}
